840dfdcc9b7df5500f6cfe82d2f9c14f30e274c9
Tres Morin <tresmorin@gmail.com>
2025-06-06 14:23:08 -0500

    Update

--- src/pages/GsapFromTo.jsx
+++ src/pages/GsapFromTo.jsx
@@ -1,5 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFromTo = () => {
   // TODO: Implement the gsap.fromTo() method
+  useGSAP(() => {
+    gsap.fromTo('#red-box', 
+    {
+      x: 250,
+      rotation: 0,
+      borderRadius: "0%"
+    }, 
+    {
+      x: 600,
+      y: 655,
+      repeat: -1,
+      yoyo: true,
+      borderRadius: "100%",
+      rotation: 360,
+      duration: 2,
+      ease: 'power1.inOut'
+    })
+  }, [])
 
   return (
     <main>
